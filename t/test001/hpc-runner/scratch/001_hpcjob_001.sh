c735ba62df4a2644e2185e592757df6d3179b580
Jillian Rowe <jillian.e.rowe@gmail.com>
2016-08-17 13:37:17 +0400

    trying git repo

--- t/test001/hpc-runner/scratch/001_hpcjob_001.sh
+++ t/test001/hpc-runner/scratch/001_hpcjob_001.sh
@@ -13,6 +13,6 @@ hpcrunner.pl execute_job \
 	--outdir /home/jillian/Dropbox/projects/perl/HPC-Runner-Command-Plugin-Logger-Sqlite/t/test001/hpc-runner/scratch \
 	--logname 001_hpcjob_001 \
 	--process_table /home/jillian/Dropbox/projects/perl/HPC-Runner-Command-Plugin-Logger-Sqlite/t/test001/hpc-runner/logs/2016-08-17-slurm_logs/001-process_table.md \
-	--metastr '{"batch":"001","jobname":"hpcjob_001","batch_index":"1/1","total_batches":1,"total_processes":4,"tally_commands":"1-4/4","commands":4}' \
+	--metastr '{"total_batches":1,"tally_commands":"1-4/4","batch_index":"1/1","commands":4,"jobname":"hpcjob_001","total_processes":4,"batch":"001"}' \
 	--job_plugins HPC::Runner::Command::execute_job::Plugin::Logger::Sqlite \
 	--job_plugins_opts submission_id=1
