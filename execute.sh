7e0a00e14e28c6d0ee76aff034c2ff59437f6911
jerowe <jillian.e.rowe@gmail.com>
2016-09-22 17:30:32 +0400

    "stuff"

--- execute.sh
+++ execute.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-hpcrunner.pl submit_jobs --infile t/test001/script/test001.1.sh --hpc_plugins Dummy
+hpcrunner.pl submit_jobs --infile t/test001/script/test001.1.sh --hpc_plugins Dummy,Logger::Sqlite --hpc_plugins_opts cleandb=1
 hpcrunner.pl execute_job --infile t/test001/script/test001.1.sh --job_plugins Logger::Sqlite --job_plugins_opts submission_id=1
---
+++ hpc-runner/0.04/logs/2016-09-22-hpcrunner_logs/001-process_table.md
@@ -0,0 +1,4 @@
+|0.04||job||22981|0|0 years, 00 months, 0 days, 00 hours, 00 minutes, 00 seconds|
+|0.04||job||22979|0|0 years, 00 months, 0 days, 00 hours, 00 minutes, 05 seconds|
+|0.04||job||22980|0|0 years, 00 months, 0 days, 00 hours, 00 minutes, 05 seconds|
+|0.04||job||22982|0|0 years, 00 months, 0 days, 00 hours, 00 minutes, 05 seconds|
